21cd5a2443a037bbc25379c13ba250c14ac9d7a1
suleboss <walesolih@gmail.com>
2025-05-28 22:58:19 +0100

    Loop to languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+const lines = ("C is fun", "Python is cool", "JvavScript is amazing");
+for (let i = 0; i <lines.length; i++ ) {
+    console.log(lines[i]);
+}
+
+
+/*const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}*/
