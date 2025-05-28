26adf9f755b0112cbc702239d8986b778ea785a8
suleboss <walesolih@gmail.com>
2025-05-28 21:03:22 +0100

    output of the arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,18 @@
-const args = process.arg.lenght-2;
+/*const args = process.arg.lenght-2;
 if (args === 0) {
     console.log("No argument");
 } else if (args === 1) {
     console.log("Argument found");
 } else{
     console.log("Argument found");
+}*/
+
+const args = process.argv.length - 2;
+
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
